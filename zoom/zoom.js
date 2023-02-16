c3956e8b4fedfab585b50fab02714e686038fefe
bluey <114776970+blueywashere@users.noreply.github.com>
2023-02-16 19:44:35 +0000

    Update zoom.js

--- zoom/zoom.js
+++ zoom/zoom.js
@@ -1,1 +1,19 @@
-
+let zoom = 1;
+let zoomed = false;
+document.addEventListener("mousedown", function(event) {
+    if (event.button === 2 && !zoomed) {
+        event.preventDefault();
+        zoom = 2;
+        document.body.style.transform = `scale(${zoom})`;
+        document.body.style.overflow = "hidden";
+        zoomed = true;
+    }
+});
+document.addEventListener("mouseup", function(event) {
+    if (event.button === 2 && zoomed) {
+        zoomed = false;
+        zoom = 1;
+        document.body.style.transform = `scale(${zoom})`;
+        document.body.style.overflow = "auto";
+    }
+});
