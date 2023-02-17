dba05980708d264dbfe7325ca9f7ab27331c6d87
bluey <114776970+blueywashere@users.noreply.github.com>
2023-02-17 14:53:41 +0000

    Update zoom_no_hold.js

--- zoom no hold/zoom_no_hold.js
+++ zoom no hold/zoom_no_hold.js
@@ -1,14 +1,15 @@
 let zoom = 1;
 let zoomed = false;
 
-document.addEventListener("click", function(event) {
-    if (!zoomed) {
+document.addEventListener("mousedown", function(event) {
+    if (event.button === 2 && !zoomed) {
         event.preventDefault();
         zoom = 2;
         document.body.style.transform = `scale(${zoom})`;
         document.body.style.overflow = "hidden";
         zoomed = true;
-    } else {
+    } else if (event.button === 2 && zoomed) {
+        event.preventDefault();
         zoomed = false;
         zoom = 1;
         document.body.style.transform = `scale(${zoom})`;
